fb5e24518bb87344ca958283d4db0df0d18099e6
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2021-07-03 14:45:39 -0300

    Add challenge

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -1,7 +1,27 @@
+// Exercicio 4
 const selectEl = document.querySelector("#filtro-da-foto");
+const fotoEl = document.querySelector(".foto-anotada > img");
 
-selectEl.addEventListener("change", e => {
-    const fotoEl = document.querySelector(".foto-anotada > img");
+selectEl.addEventListener("change", e => fotoEl.style.filter = e.currentTarget.value);
 
-    fotoEl.style.filter = e.currentTarget.value;
+// Desafio
+const fileEl = document.querySelector("#imagem");
+fileEl.addEventListener("change", e => {
+    
+    const file = e.target.files[0];
+    if (!file.type) {
+        console.log("Erro: A propriedade File.type não parece ser suportada neste navegador.");
+        return;
+    }
+    if (!file.type.match('image.*')) {
+        console.log("Erro: A imagem selecionada não parece ser uma imagem");
+        return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.addEventListener("load", e => {
+        fotoEl.src = e.target.result;
+        console.log("baU BAU")
+    });
+    fileReader.readAsDataURL(file);
 });
